1555048af7f400afa26aa194e9c7be8e2573d438
Lucas Cieslak <lukasz.cieslak69@gmail.com>
2018-11-26 00:04:38 +0100

    added not_navie_navigate function for custom navigation for ships  - used it only for ships with collecting status for now

--- bot_new_trial.py
+++ bot_new_trial.py
@@ -1,29 +1,38 @@
 # Import the Halite SDK, which will let you interact with the game.
 import hlt
-
-# This library contains constant values.
 from hlt import constants
-
-# This library contains direction metadata to better interface with the game.
 from hlt.positionals import Direction
-
-# This library allows you to generate random numbers.
+from hlt import game_map
 import random
 
 # Logging allows you to save messages for yourself. This is required because the regular STDOUT
 #   (print statements) are reserved for the engine-bot communication.
 import logging
 
+
+def not_naive_navigate(turtle, destination):
+    """
+    Returns a singular safe move towards the destination.
+
+    :param turtle: The ship to move.
+    :param destination: Ending position
+    :return: A direction.
+    """
+    # No need to normalize destination, since get_unsafe_moves
+    # does that
+    for changes in game_map.get_unsafe_moves(turtle.position, destination):
+        target_pos = turtle.position.directional_offset(changes)
+        # if not game_map[target_pos].is_occupied:
+        game_map[target_pos].mark_unsafe(turtle)
+        return changes
+
+    return Direction.Still
+
+
 """ <<<Game Begin>>> """
 
 # This game object contains the initial game state.
 game = hlt.Game()
-# Get size of the map (for future map loops)
-gm_height = game.game_map.height
-gm_width = game.game_map.width
-ship_states = {}
-# Create a direction order list: move up, down, left, right or hold still
-direction_order = [Direction.North, Direction.South, Direction.East, Direction.West, Direction.Still]
 # At this point "game" variable is populated with initial map data.
 # This is a good place to do computationally expensive start-up pre-processing.
 # As soon as you call "ready" function below, the 2 second per turn timer will start.
@@ -34,52 +43,50 @@ game.ready("Teldrin89_Python_Bot")
 logging.info("Successfully created bot! My Player ID is {}.".format(game.my_id))
 
 """ <<<Game Loop>>> """
-
+# dictionary created outside of game loop
+ship_states = {}
 while True:
+    # This loop handles each turn of the game. The game object changes every turn, and you refresh that state by
+    #   running update_frame().
     game.update_frame()
+    # You extract player metadata and the updated map metadata here for convenience.
     me = game.me
     game_map = game.game_map
+
+    # A command queue holds all the commands you will run this turn. You build this list up and submit it at the
+    #   end of the turn.
     command_queue = []
-    # Generate the list of ships to get the number of them
-    number_of_ships = len(me.get_ships())
+    # direction order list: move up, down, left, right or stand still
+    direction_order = [Direction.North, Direction.South, Direction.East, Direction.West, Direction.Still]
     # added position choices that will contain a physical coordinate of a map - created to avoid ships crashing
-    # TODO - not used for now to avoid crashing but still used for movement
     position_choices = []
-    # Created a dictionary that will contain all ships current positions
-    ships_positions = {}
-    # Run for loop for all ships on map
-    for ship in me.get_ships():
-        # Populate the dictionary w key:value pair of ship id and ship position
-        ships_positions[ship.id] = ship.position
 
     for ship in me.get_ships():
-        # Populate dictionary if ship is collecting
+        # populate dictionary if ship is collecting
         if ship.id not in ship_states:
             ship_states[ship.id] = "collecting"
 
         # Introduce choices for the given ship
         position_options = ship.position.get_surrounding_cardinals() + [ship.position]
-        # Create a position dictionary, will map movement to the map coordinates: {(0,1) : (19,48)}
+        # create a position dictionary, will map movement to the map coordinates
         position_dict = {}
-        # Create a halite dictionary that will map the position with halite amount at that position
-        # {(0,1) : 550}
+        # create a halite dictionary that will map the position with halite amount at that position
         halite_dict = {}
-        # Populate the position dictionary for current ship with actual coordinates
         for n, direction in enumerate(direction_order):
             position_dict[direction] = position_options[n]
-        # Populate the halite dictionary, taking the halite amount for each position from position
-        # dictionary and also varying based on the selected direction promoting standing still (to avoid
-        # constant movements from one position to the other - TODO consider putting additional if for small
-        # differences in surrounding positions)
+
         for direction in position_dict:
             position = position_dict[direction]
             halite_amount = game_map[position].halite_amount
-            if direction == Direction.Still:
-                halite_dict[direction] = halite_amount * 3
+            if position_dict[direction] not in position_choices:
+                if direction == Direction.Still:
+                    halite_dict[direction] = halite_amount * 3
+                else:
+                    halite_dict[direction] = halite_amount
             else:
-                halite_dict[direction] = halite_amount
+                logging.info("attempting to move to the same spot\n")
 
-        # Check if given ship has a "depositing" or "collecting" tag
+        # if loop to check if given ship has a "depositing" or "collecting" tag
         if ship_states[ship.id] == "depositing":
             # in case of depositing the ship has to move towards shipyard to drop halite
             move = game_map.naive_navigate(ship, me.shipyard.position)
@@ -88,26 +95,25 @@ while True:
             # add the move to command que
             command_queue.append(ship.move(move))
             # added the logic for ship after it reaches shipyard and drops off halite
-            if ship.position == me.shipyard.position:
+            if move == Direction.Still:
                 ship_states[ship.id] = "collecting"
         elif ship_states[ship.id] == "collecting":
-            # Create a directional choice based on the max halite in halite dictionary
+            # checks if halite on the map is lower than 10% of max halite - it moves to location with more
             directional_choice = max(halite_dict, key=halite_dict.get)
             position_choices.append(position_dict[directional_choice])
-            command_queue.append(ship.move(game_map.naive_navigate(ship, position_dict[directional_choice])))
-            # If ship has more than 90% of it's capacity the tag will change to depositing
-            if ship.halite_amount > constants.MAX_HALITE * 0.90:
+            command_queue.append(ship.move(not_naive_navigate(ship, position_dict[directional_choice])))
+            # if the ship has more than a 93% of max capacity - this to avoid the bad collisions with enemy ship
+            # that could cause a whole max halite amount of ship lost
+            if ship.halite_amount > constants.MAX_HALITE*0.92:
                 # change of ship tag to "depositing"
                 ship_states[ship.id] = "depositing"
 
-    # If there is no ship then spawn one - up until 15 ships in total
     # If the game is in the first 200 turns and you have enough halite, spawn a ship.
     # Don't spawn a ship if you currently have a ship at port, though - the ships will collide.
-
-    if number_of_ships < 20:
-        if game.turn_number <= 150 and me.halite_amount >= constants.SHIP_COST and not game_map[me.shipyard].is_occupied:
-            command_queue.append(me.shipyard.spawn())
+    if game.turn_number <= 150 and me.halite_amount >= constants.SHIP_COST and not game_map[me.shipyard].is_occupied:
+        command_queue.append(me.shipyard.spawn())
 
     # Send your moves back to the game environment, ending this turn.
     game.end_turn(command_queue)
+
 
